82e07fd47382ed1f0370359444a2911acd63b627
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-21 21:58:36 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 52;
+            int c = b / a;
+            int d = b % a;
+            Console.WriteLine(" Заполненных рядов " + c);
+            Console.WriteLine(" Осталось картинок " + d);
+        }
+    }
+}
